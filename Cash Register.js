8ffac471503742fe750d88ac3a35d49c8435e878
Mohd Zeefan <mhdzeefan@gmail.com>
2019-10-01 23:11:29 +0530

    cidObj updated

--- Cash Register.js
+++ Cash Register.js
@@ -6,14 +6,14 @@ function checkCashRegister(price, cash, cid) {
   let status = { status: "", change: [] };
 
   let cidObj = {
-    PENNY: 0.01,
-    NICKEL: 0.05,
-    DIME: 0.1,
-    QUARTER: 0.25,
-    ONE: 1,
-    FIVE: 5,
-    TEN: 10,
-    TWENTY: 20,
+    "PENNY": 0.01,
+    "NICKEL": 0.05,
+    "DIME": 0.1,
+    "QUARTER": 0.25,
+    "ONE": 1,
+    "FIVE": 5,
+    "TEN": 10,
+    "TWENTY": 20,
     "ONE HUNDRED": 100
   };
 
